259e47d4e6f6fee89c798018c58e12a65bed7bb7
nlovas <nlovas@ualberta.ca>
2017-07-20 23:17:24 -0600

    previewing works, next step is the hardest!

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# MozaicMe
-A new personal project, oooo! More info coming soon ;)
+# MozaicMe  
+
+A personal practise project! I'm using:
+-Django  
+-HTML  
+-Javascript & JQuery  
+-CSS  
+-Web Mining  
+...and more! More info coming soon  
+
+A list of resources that I found helpful:
+
+How to preview an image before uploading:  
+https://stackoverflow.com/a/4459419
+Ivan Baev (https://stackoverflow.com/users/344437/ivan-baev) On Stack Overflow  
+
+Making $.change() work:
+https://stackoverflow.com/a/19194194
+by Dhaval Marthak (https://stackoverflow.com/users/2157321/dhaval-marthak) on Stack Overflow  
--- home/static/uploader.js
+++ home/static/uploader.js
@@ -1,5 +1,21 @@
+var maxPrevWidth = 100;
+var maxPrevHeight = 200;
+
+/*
+The following code snippet is from https://stackoverflow.com/a/14731922
+ Jason J Nathan (https://stackoverflow.com/users/382536/jason-j-nathan) On Stack Overflow
+*//*
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+	
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+	console.log(ratio);
+
+    return { width: srcWidth*ratio, height: srcHeight*ratio };
+ }
+*/
+ 
  /*
- The following code snipper is from https://stackoverflow.com/a/4459419
+ The following code snippet is from https://stackoverflow.com/a/4459419
  Ivan Baev (https://stackoverflow.com/users/344437/ivan-baev) On Stack Overflow
  -------------------
  */
@@ -8,18 +24,21 @@
             var reader = new FileReader();
             
             reader.onload = function (e) {
-                $('#preview').attr('src', e.target.result);
-            }
+               $('#preview').attr('src', e.target.result);
+			}
             
             reader.readAsDataURL(input.files[0]);
-        }
+        } else{
+			$('#preview').attr('src', "");
+		}
     }
     /*fixed the broken code with this solution:
 	https://stackoverflow.com/a/19194194
 	by Dhaval Marthak (https://stackoverflow.com/users/2157321/dhaval-marthak) on Stack Overflow
 	*/
     $(document).on('change','#uploadImage',function(event){
-		console.log("ok");
         readURL(this);
+		console.log("change!");
     });
 //--------------------
+
